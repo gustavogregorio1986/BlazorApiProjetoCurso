fc74e34f177537ff9ada63713331bee4dfeed413
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-16 12:07:35 -0300

    Add project files.

---
+++ BlazorApiProjetoCurso/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorApiProjetoCurso.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApiProjetoCurso.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
