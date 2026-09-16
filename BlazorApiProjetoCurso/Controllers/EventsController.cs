1cea1b23beed31d0f0546debec42a695dd478d2e
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-16 12:23:28 -0300

    Desenvolvendo o endpoint da lista e buscarPorId no projeto Blazor

---
+++ BlazorApiProjetoCurso/Controllers/EventsController.cs
@@ -0,0 +1,38 @@
+﻿using BlazorApiProjetoCurso.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApiProjetoCurso.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EventsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            var evets = _context.Events.OrderBy(e => e.Id).ToList();
+            return Ok(evets);
+        }
+
+        [HttpGet("{id}")] 
+        public IActionResult GetEvent(int id) 
+        {
+            var events = _context.Events.Find(id);
+
+            if(events == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(events);
+        }
+    }
+}
--- BlazorApiProjetoCurso/Program.cs
+++ BlazorApiProjetoCurso/Program.cs
@@ -1,5 +1,7 @@
 using BlazorApiProjetoCurso.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
