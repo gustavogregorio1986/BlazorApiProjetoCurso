678e6147bcbb62d82c54da91b16c1787ba021cde
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-18 15:55:35 -0300

    Desenvolver a lista no projeot Blazor

--- BlazorApiProjetoCurso/Program.cs
+++ BlazorApiProjetoCurso/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
