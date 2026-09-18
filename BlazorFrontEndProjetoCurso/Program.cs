ae2d125f60c336ce6763afa3249eb76510a6f414
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-18 16:31:00 -0300

    Desenvolver o delete no projeto Blazor

--- BlazorFrontEndProjetoCurso/Program.cs
+++ BlazorFrontEndProjetoCurso/Program.cs
@@ -1,10 +1,12 @@
 using BlazorFrontEndProjetoCurso;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CurrieTechnologies.Razor.SweetAlert2; // <--- Não esqueça do using no topo
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSweetAlert2();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7253") });
 
