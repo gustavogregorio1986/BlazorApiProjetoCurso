1a18cacf45c5175c0dc4050b8e8edc07cb04f80a
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-18 17:54:56 -0300

    Desenvolvimento do layout de cadastro no projeto Blazor

---
+++ BlazorFrontEndProjetoCurso/Dto/EventDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFrontEndProjetoCurso.Dto
+{
+    public class EventDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+        public bool AllDay { get; set; }
+    }
+}
