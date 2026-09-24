c934a6ec59365e436f6c91bfff0509b82267eb97
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-09-24 17:23:41 -0300

    Desenvolver o calendario na tela no projeot Blazor

---
+++ BlazorFrontEndProjetoCurso/wwwroot/js/agenda.js
@@ -0,0 +1,36 @@
+﻿function loadAgenda(eventsUrl) {
+    var calendarEl = document.getElementById("eventAgendaId");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        initialView: 'dayGridMonth',
+        themeSystem: 'Yeti',
+        contentHeight: "auto",
+        events: eventsUrl,
+        selectable: true,
+        editable: true,
+        locale: 'pt',
+        buttonText: {
+            prev: 'Anterior',
+            next: 'Seguinte',
+            month: 'Mês',
+            today: 'Hoje',
+            week: 'Semana',
+            day: 'dia',
+            list: 'Lista',
+        },
+        select: handleSelectEvent,
+        eventClick: handleEventClick,
+        eventChange: handleEventChange,
+    });
+    calendar.render();
+
+    function handleSelectEvent() { }
+
+    function handleEventClick() { }
+
+    function handleEventChange() { }    
+}
